77402ce1bf67c0d2146f277269344c9795b12a92
anthonysapp <anthony@reli.sh>
2024-07-12 12:14:46 -0400

    added sandbox config

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import config from 'dill-pixel/config/vite';
 
 config.server.port = undefined;
 config.server.host = process.env.CODESANDBOX_SSE ? '0.0.0.0' : undefined;
-config.server.open
+config.server.open = false;
 
 export default config;
