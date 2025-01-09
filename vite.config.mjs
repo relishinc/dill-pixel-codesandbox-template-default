84b9e7519fa1856ed933ac3cb1bf651327b93a33
Anthony Sapp <anthonysapp@gmail.com>
2025-01-09 13:16:22 -0500

    fix: codesandbox settings

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,15 @@
 import config from 'dill-pixel/config/vite';
 
-config.server.port = undefined;
-config.server.host = process.env.CODESANDBOX_SSE ? '0.0.0.0' : undefined;
+config.server.port = 3000;
+config.server.strictPort = true;
+if (!config.server.hmr) {
+  config.server.hmr = {}
+}
+config.server.hmr.port = 443;
+config.server.hmr.protocol = 'wss';
+config.server.hmr.clientPort = 3000
+config.server.hmr.host ='0.0.0.0'
+config.server.host = '0.0.0.0'
 config.server.open = false;
 
 export default config;
