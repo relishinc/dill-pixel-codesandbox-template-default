c6977cd80105cc1f1845b4da769a61494f642444
Anthony Sapp <anthonysapp@gmail.com>
2025-01-09 13:25:57 -0500

    fix: codesandbox settings

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,9 +7,5 @@ if (!config.server.hmr) {
 }
 config.server.hmr.port = 443;
 config.server.hmr.protocol = 'wss';
-config.server.hmr.clientPort = 3000
-config.server.hmr.host ='0.0.0.0'
-config.server.host = '0.0.0.0'
-config.server.open = false;
 
 export default config;
